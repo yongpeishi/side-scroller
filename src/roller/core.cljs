cce23b4f5edddb76df247d7e20eb4ad2c8bb1c2d
yongpeishi <yongpeishi@gmail.com>
2016-12-09 16:23:38 +1100

    roller now move smoothly upwards

--- src/roller/core.cljs
+++ src/roller/core.cljs
@@ -7,14 +7,15 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state
+  (atom {:roller-y 312}))
 
-(defn hello-world []
-  [:div
-   [:h1 (:text @app-state)]
-   [:div.roller]])
+(defn game-world []
+  (let [{:keys [roller-y]} @app-state]
+    [:div
+     [:div.roller {:style {:top (str roller-y "px")} }]]))
 
-(reagent/render-component [hello-world]
+(reagent/render-component [game-world]
                           (. js/document (getElementById "app")))
 
 (defn on-js-reload []
@@ -22,3 +23,24 @@
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
+
+(defn update-roller []
+  (swap! app-state (fn [{:keys [roller-y]}]
+                     (if (<= roller-y 100)
+                       {:roller-y 100}
+                       {:roller-y (- roller-y 10)}))))
+
+(defn time-loop [timestamp]
+  (update-roller)
+  (.requestAnimationFrame js/window time-loop)
+  )
+
+(aset js/document "onkeypress"
+      (fn [e]
+        (if (= 32 (aget e "charCode"))
+          (do
+           (println "enter pressed")
+           (.requestAnimationFrame js/window time-loop))
+          ;;(update-roller)
+          (println "no"))))
+
